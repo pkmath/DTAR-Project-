b50e252f94bf0ccf366aec23267a83078bf7a38c
jaimugarte <jaimugarte@gmail.com>
2019-03-14 10:06:42 +0000

    Folder structure and other files

---
+++ scripts/charge_libraries.R
@@ -0,0 +1,23 @@
+charge_libraries <- function(){
+  packages <- c("ggplot2", "DT", "httr", "dplyr", "rvest", "readxl",
+                "googledrive","RCurl","openssl","taskscheduleR","sendmailR",
+                "mailR","R.utils","shiny","shinydashboard")
+  if (length(setdiff(packages, rownames(installed.packages()))) > 0) {
+    install.packages(setdiff(packages, rownames(installed.packages())))  
+  }
+  
+  library(shinydashboard)
+  library(DT)
+  library(httr)
+  library(dplyr)
+  library(rvest)
+  library(readxl)
+  library(googledrive)
+  library(RCurl)
+  library(openssl)
+  library(taskscheduleR)
+  library(sendmailR)
+  library(mailR)
+  library(R.utils)
+}
+
---
+++ scripts/datatable.R
@@ -0,0 +1,35 @@
+library(RSQLite)
+devtools::install_github('jbryer/DTedit')
+DTedit::dtedit_demo()
+library(shiny)
+library(DT)
+shinyApp(
+  ui = fluidPage(
+    fluidRow(
+      verbatimTextOutput("value1"),
+      column(12,
+             DT::dataTableOutput('table'),  tags$script(HTML('$(document).on("click", "input", function () {
+                       var checkboxes = document.getElementsByName("selected");
+                       var checkboxesChecked = [];
+                       for (var i=0; i<checkboxes.length; i++) {
+                       if (checkboxes[i].checked) {
+                       checkboxesChecked.push(checkboxes[i].value);
+                      }
+                      }
+                     Shiny.onInputChange("checked_rows",checkboxesChecked);  })'))
+      ))),
+  server = function(input, output) {
+    library(DT)
+    library(glue)
+    output$value1 <- renderPrint({ input$checked_rows }) 
+    
+    output$table <- DT::renderDataTable({
+      iris$checked<-''
+      iris$checked[2:5]<-'checked=\"checked\"'
+      iris[["Select"]]<-glue::glue('<input type="checkbox" name="selected" {iris$checked} value="{1:nrow(iris)}"><br>')
+      datatable(iris,escape=F,rownames=F,  class = 'cell-border compact', 
+                options=list(ordering=T,autowidth=F,scrollX = TRUE,
+                             columnDefs = list(list(className = 'dt-center', targets = "_all"))
+                ),
+                selection="none"
+      ) })})
